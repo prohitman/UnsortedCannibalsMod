07295a1337998bc7a7e6b79eb60da88672979434
Amine Lahnin <lahnamin@gmail.com>
2024-06-07 00:00:05 +0100

    Fixed Bugs
    -Added Functionality to Sinister Skull
    -Tweaked Fall Trap gui item model.
    -Tweaked Sharpened Bones item model displays.

--- src/main/java/com/prohitman/unsortedcannibals/common/blocks/SinisterSkull.java
+++ src/main/java/com/prohitman/unsortedcannibals/common/blocks/SinisterSkull.java
@@ -1,28 +1,46 @@
 package com.prohitman.unsortedcannibals.common.blocks;
 
+import com.prohitman.unsortedcannibals.core.init.ModEffects;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.SkullBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.FluidState;
 
 public class SinisterSkull extends HorizontalDirectionalBlock {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
 
     public SinisterSkull(Properties pProperties) {
         super(pProperties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(PERSISTENT, false));
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        if(!state.getValue(PERSISTENT)){
+            player.addEffect(new MobEffectInstance(ModEffects.LIVE_BAIT.get(), 600, 1), player);
+        }
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
 
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection());
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection()).setValue(PERSISTENT, true);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(FACING);
+        pBuilder.add(FACING, PERSISTENT);
     }
 }
